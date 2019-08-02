40e2067c0e452c61bfade48d496f562805d1905a
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 20:26:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main()
- {
-  unsigned int a ;
-  cout<<"Your salary($): ";
-  cin>>a;
-  switch (a)
-   {  
-    case 0 ... 1000:
-    cout <<"Rabotai bolshe";
-    break;
+{
+    unsigned int a ;
+    cout<<"Your salary($): ";
+    cin>>a;
+    switch (a)
+    {  
+        case 0 ... 1000:
+            cout <<"Rabotai bolshe";
+            break;
       
-    case 1001 ... 1000000:
-    cout <<"Horosho";
-    break;
+        case 1001 ... 1000000:
+            cout <<"Horosho";
+            break;
       
-    case 1000001 ... 4294967295:
-    cout <<"Ti millioner!";
-    break;
-   }
-  cout <<"...no ti molodec";
-  return 0;
- }
+        case 1000001 ... 4294967295:
+            cout <<"Ti millioner!";
+            break;
+    }
+    cout <<"...no ti molodec";
+    return 0;
+}
 
